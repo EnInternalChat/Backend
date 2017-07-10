6060e46ec66137cd85897bf2ff49b9e2f1ded54f
GARENFEATHER <476217804@qq.com>
2017-07-10 14:40:06 +0800

    wtf

--- webStart/BackContent/src/main/java/backend/repository/EmployeeRepository.java
+++ webStart/BackContent/src/main/java/backend/repository/EmployeeRepository.java
@@ -3,7 +3,6 @@ package backend.repository;
 import backend.mdoel.Employee;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -11,12 +10,10 @@ import java.util.List;
  * Created by lenovo on 2017/5/7.
  */
 
-@Repository
 public interface EmployeeRepository extends MongoRepository<Employee, Long> {
     List<Employee> findByCompanyIDAndSectionID (long companyID, long sectionID);
     List<Employee> findByCompanyID(long companyID);
     List<Employee> findByCompanyIDAndSectionID (long companyID, long sectionID, Pageable pageable);
     List<Employee> findByCompanyID(long companyID, Pageable pageable);
-    List<Employee> findByCompanyIDAndName(long companyID, String name);
     List<Employee> findByName(String name);
 }
