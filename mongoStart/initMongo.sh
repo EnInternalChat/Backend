305076906f7a5c9b37490ce1c73129daf5c96194
GARENFEATHER <476217804@qq.com>
2017-07-14 16:12:21 +0800

    wocao wtf bug

--- mongoStart/initMongo.sh
+++ mongoStart/initMongo.sh
@@ -1,6 +1,7 @@
 mongoimport -h localhost -p 27017 -d EnInternalChatTest -c company /docker-entrypoint-initdb.d/0-company.json
 mongoimport -h localhost -p 27017 -d EnInternalChatTest -c employee /docker-entrypoint-initdb.d/0-employee.json
 mongoimport -h localhost -p 27017 -d EnInternalChatTest -c section /docker-entrypoint-initdb.d/0-section.json
+mongoimport -h localhost -p 27017 -d EnInternalChatTest -c idManager /docker-entrypoint-initdb.d/0-idManager.json
 mongoimport -h localhost -p 27017 -d EnInternalChatTest -c notification /docker-entrypoint-initdb.d/0-notification.json
 mongoimport -h localhost -p 27017 -d EnInternalChat -c company /docker-entrypoint-initdb.d/0-company.json
 mongoimport -h localhost -p 27017 -d EnInternalChat -c employee /docker-entrypoint-initdb.d/0-employee.json
